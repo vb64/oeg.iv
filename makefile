f64d687864e74b342aeb2c7c476f9c9ecfde9029
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-12-09 19:11:13 +0400

    Initial (#1)

---
+++ makefile
@@ -0,0 +1,53 @@
+.PHONY: all setup tests dist
+# make tests >debug.log 2>&1
+
+ifeq ($(OS),Windows_NT)
+PYTHON = venv/Scripts/python.exe
+PTEST = venv/Scripts/pytest.exe
+COVERAGE = venv/Scripts/coverage.exe
+else
+PYTHON = ./venv/bin/python
+PTEST = ./venv/bin/pytest
+COVERAGE = ./venv/bin/coverage
+endif
+
+SOURCE = oeg_iv
+TESTS = tests
+PYTEST = $(PTEST) --cov=$(SOURCE) --cov-report term:skip-covered
+
+all: tests
+
+test:
+	$(PYTEST) -s --cov-append $(TESTS)/test/$(T)
+	$(COVERAGE) html --skip-covered
+
+tests: flake8 lint
+	$(PYTEST) --durations=5 $(TESTS)
+	$(COVERAGE) html --skip-covered
+
+flake8:
+	$(PYTHON) -m flake8 --max-line-length=120 $(TESTS)
+	$(PYTHON) -m flake8 --max-line-length=120 $(SOURCE)
+
+lint:
+	$(PYTHON) -m pylint $(TESTS)/test
+	$(PYTHON) -m pylint $(SOURCE)
+
+dist:
+	$(PYTHON) setup.py sdist bdist_wheel
+
+upload_piptest: tests dist
+	$(PYTHON) -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+upload_pip: tests dist
+	$(PYTHON) -m twine upload dist/*
+
+setup: setup_python setup_pip
+
+setup_pip:
+	$(PYTHON) -m pip install --upgrade pip
+	$(PYTHON) -m pip install -r tests/requirements.txt
+
+setup_python:
+	$(PYTHON_BIN) -m pip install virtualenv
+	$(PYTHON_BIN) -m virtualenv ./venv
---
+++ oeg_iv/csvfile/row.py
@@ -0,0 +1,408 @@
+# -*- coding: windows-1251 -*-
+"""
+InspectionViewer export csv file data row
+"""
+from .. import (
+  Error, ObjectClass, TypeMarker, TypeHorWeld, COMMON, LINEOBJ, SEAM, DEFEKTS, MARKERS, DefektSide
+)
+
+
+def iv_bool(val):
+    """
+    return bool value for iv csv
+    """
+    return 'ИСТИНА' if val else 'ЛОЖЬ'
+
+
+def to_int(text):
+    """
+    convert text to int
+    """
+    return int(round(float(text.strip().replace(',', '.')), 0))
+
+
+def reverse_orient(orient_str):
+    """
+    reverse orientation of string in format "hours,minutes"
+    """
+    if not orient_str:
+        return orient_str
+
+    hours_str = orient_str
+    minutes_str = '0'
+    if ',' in orient_str:
+        hours_str, minutes_str = orient_str.split(',')
+
+    if hours_str == '12':
+        hours_str = '0'
+
+    if len(minutes_str) == 1:
+        minutes_str += '0'
+
+    minutes = 60 - int(minutes_str)
+    hours = 12 - int(hours_str)
+    if minutes == 60:
+        minutes = 0
+    else:
+        hours -= 1
+
+    if minutes < 10:
+        minutes = '0{}'.format(minutes)
+
+    return "{},{}".format(hours, minutes)
+
+
+class Row(object):  # pylint: disable=too-many-instance-attributes
+    """
+    row of export csv file
+    """
+    def __init__(self):
+        self.dist_od = None
+        self.type_object = None
+        self.object_code = 0
+        self.object_name = ''
+        self.object_code_t = ''
+        self.marker = iv_bool(False)
+        self.length = ''
+        self.width = ''
+        self.depth_min = ''
+        self.depth_max = ''
+        self.orient_td = ''
+        self.orient_bd = ''
+        self.mpoint_orient = ''
+        self.mpoint_dist = ''
+        self.type_def = ''
+        self.dist_ml = ''
+        self.dist_mr = ''
+        self.dist_stl = ''
+        self.dist_str = ''
+        self.link_stl = ''
+        self.link_str = ''
+        self.link_ml = ''
+        self.link_mr = ''
+        self.comments = ''
+        self.latitude = ''
+        self.longtitude = ''
+        self.altitude = ''
+
+    def set_geo(self, latitude, longtitude, altitude):
+        """
+        set geo coords for object
+        """
+        self.latitude = latitude
+        self.longtitude = longtitude
+        self.altitude = altitude
+
+        return self
+
+    @classmethod
+    def with_dist(cls, distanse, latitude='', longtitude='', altitude=''):
+        """
+        construct row as common object with dist and geo
+        """
+        obj = cls()
+        obj.dist_od = int(distanse)
+
+        return obj.set_geo(latitude, longtitude, altitude)
+
+    @classmethod
+    def as_common(cls, distanse, typ, latitude='', longtitude='', altitude=''):
+        """
+        construct row as common object
+        """
+        obj = cls.with_dist(distanse, latitude, longtitude, altitude)
+        obj.type_object = typ
+        obj.object_code_t = COMMON[obj.type_object]
+        return obj
+
+    @classmethod
+    def as_weld(cls, distanse, latitude='', longtitude='', altitude=''):
+        """
+        construct row as weld object
+        """
+        return cls.as_common(distanse, ObjectClass.WELD, latitude, longtitude, altitude)
+
+    @classmethod
+    def as_thick(cls, distanse, thick, latitude='', longtitude='', altitude=''):
+        """
+        construct row as thickness change object
+        """
+        obj = cls.as_common(distanse, ObjectClass.THICK, latitude, longtitude, altitude)
+        obj.depth_max = thick
+        return obj
+
+    @classmethod
+    def as_category(cls, distanse, category, latitude='', longtitude='', altitude=''):
+        """
+        construct row as pipeline category object
+        """
+        obj = cls.as_common(distanse, ObjectClass.PIPELINE_CATEGORY, latitude, longtitude, altitude)
+        obj.depth_max = category
+        return obj
+
+    @classmethod
+    def as_seam(cls, distanse, typ, orient1, orient2):
+        """
+        construct row as seam object with given typ
+        """
+        if typ not in SEAM:
+            raise Error("Wrong seam type: {}".format(typ))
+
+        obj = cls.with_dist(distanse, '', '', '')
+        obj.type_object = ObjectClass.HOR_WELD
+        obj.object_code = typ
+        obj.object_code_t = SEAM[obj.object_code]
+
+        if obj.object_code in [TypeHorWeld.HORIZONTAL, TypeHorWeld.SPIRAL, TypeHorWeld.SECOND]:
+            obj.orient_td = str(orient1)
+
+        if obj.object_code in [TypeHorWeld.SECOND]:
+            obj.orient_bd = str(orient2)
+
+        return obj
+
+    @classmethod
+    def as_lineobj(cls, distanse, typ, name, comment, latitude='', longtitude='', altitude=''):
+        """
+        construct row as line object
+        """
+        obj = cls.with_dist(distanse, latitude, longtitude, altitude)
+        obj.type_object = ObjectClass.MARKER
+        obj.object_code = typ
+        obj.object_code_t = LINEOBJ[typ]
+        obj.object_name = name
+        obj.marker = iv_bool(typ in MARKERS)
+        obj.comments = comment
+
+        return obj
+
+    @classmethod
+    def as_defekt(
+      cls, distanse, typ, length, width, depth, orient1, orient2, comment,
+      latitude='', longtitude='', altitude=''
+    ):
+        """
+        construct row as defekt object
+        """
+        if typ not in DEFEKTS:
+            raise Error("Wrong defekt type: {}".format(typ))
+
+        depth_int = to_int(depth)
+
+        obj = cls.with_dist(distanse, latitude, longtitude, altitude)
+        obj.type_object = ObjectClass.DEFEKT
+        obj.object_code = typ
+        obj.object_code_t = DEFEKTS[typ]
+        obj.orient_td = str(orient1)
+        obj.orient_bd = str(orient2)
+
+        obj.length = length
+        obj.width = width
+        obj.depth_min = depth_int
+        obj.depth_max = depth_int
+        obj.type_def = DefektSide.UNKNOWN
+
+        obj.comments = comment
+
+        return obj
+
+    @classmethod
+    def from_csv_row(cls, row):
+        """
+        construct from csv row
+        """
+        obj = cls()
+
+        obj.dist_od = row[0]
+        obj.type_object = row[1]
+        obj.object_code = row[2]
+        obj.object_name = row[3]
+        obj.object_code_t = row[4]
+        obj.marker = row[5]
+        obj.length = row[6]
+        obj.width = row[7]
+        obj.depth_min = row[8]
+        obj.depth_max = row[9]
+        obj.orient_td = row[10]
+        obj.orient_bd = row[11]
+        obj.mpoint_orient = row[12]
+        obj.mpoint_dist = row[13]
+        obj.type_def = row[14]
+        obj.dist_ml = row[15]
+        obj.dist_mr = row[16]
+        obj.dist_stl = row[17]
+        obj.dist_str = row[18]
+        obj.link_stl = row[19]
+        obj.link_str = row[20]
+        obj.link_ml = row[21]
+        obj.link_mr = row[22]
+        obj.comments = row[23]
+        obj.latitude = row[24]
+        obj.longtitude = row[25]
+        obj.altitude = row[26]
+
+        return obj
+
+    def values(self):
+        """
+        column values for row
+        """
+        return [
+          self.dist_od,
+          self.type_object,
+          self.object_code,
+          self.object_name,
+          self.object_code_t,
+          self.marker,
+          self.length,
+          self.width,
+          self.depth_min,
+          self.depth_max,
+          self.orient_td,
+          self.orient_bd,
+          self.mpoint_orient,
+          self.mpoint_dist,
+          self.type_def,
+          self.dist_ml,
+          self.dist_mr,
+          self.dist_stl,
+          self.dist_str,
+          self.link_stl,
+          self.link_str,
+          self.link_ml,
+          self.link_mr,
+          self.comments,
+          self.latitude,
+          self.longtitude,
+          self.altitude,
+        ]
+
+    def copy(self):
+        """
+        create copy of row
+        """
+        obj = Row()
+
+        obj.dist_od, \
+            obj.type_object, \
+            obj.object_code, \
+            obj.object_name, \
+            obj.object_code_t, \
+            obj.marker, \
+            obj.length, \
+            obj.width, \
+            obj.depth_min, \
+            obj.depth_max, \
+            obj.orient_td, \
+            obj.orient_bd, \
+            obj.mpoint_orient, \
+            obj.mpoint_dist, \
+            obj.type_def, \
+            obj.dist_ml, \
+            obj.dist_mr, \
+            obj.dist_stl, \
+            obj.dist_str, \
+            obj.link_stl, \
+            obj.link_str, \
+            obj.link_ml, \
+            obj.link_mr, \
+            obj.comments, \
+            obj.latitude, \
+            obj.longtitude, \
+            obj.altitude = self.values()
+
+        return obj
+
+    @property
+    def is_category(self):
+        """
+        row is pipeline category object
+        """
+        return int(self.type_object) == ObjectClass.PIPELINE_CATEGORY
+
+    @property
+    def is_thick(self):
+        """
+        row is wall thick change object
+        """
+        return int(self.type_object) == ObjectClass.THICK
+
+    @property
+    def is_weld(self):
+        """
+        row is weld object
+        """
+        return int(self.type_object) == ObjectClass.WELD
+
+    @property
+    def is_defect(self):
+        """
+        row is defect object
+        """
+        return int(self.type_object) == ObjectClass.DEFEKT
+
+    def reverse(self, total_length):
+        """
+        reverse dist, orientation and start point if objects with length
+        """
+        my_length = 0
+        if self.length:
+            my_length = int(self.length)
+
+        self.dist_od = str(total_length - int(self.dist_od) - my_length)
+
+        if self.mpoint_dist:
+            self.mpoint_dist = str(total_length - int(self.mpoint_dist))
+
+        tmp = self.dist_ml
+        self.dist_ml = self.dist_mr
+        self.dist_mr = tmp
+
+        tmp = self.dist_stl
+        self.dist_str = self.dist_stl
+        self.dist_stl = tmp
+
+        tmp = self.link_stl
+        self.link_stl = self.link_str
+        self.link_str = tmp
+
+        tmp = self.link_ml
+        self.link_ml = self.link_mr
+        self.link_mr = tmp
+
+        # orientatations
+        self.orient_td = reverse_orient(self.orient_td)
+        self.orient_bd = reverse_orient(self.orient_bd)
+
+        if int(self.type_object) != ObjectClass.HOR_WELD:
+            tmp = self.orient_bd
+            self.orient_bd = self.orient_td
+            self.orient_td = tmp
+
+        self.mpoint_orient = reverse_orient(self.mpoint_orient)
+
+        # object type
+        if int(self.type_object) == ObjectClass.MARKER:
+            object_code = int(self.object_code)
+            new_code = object_code
+
+            if object_code == TypeMarker.CASE_START:
+                new_code = TypeMarker.CASE_END
+            elif object_code == TypeMarker.CASE_END:
+                new_code = TypeMarker.CASE_START
+            elif object_code == TypeMarker.TURN_START:
+                new_code = TypeMarker.TURN_END
+            elif object_code == TypeMarker.TURN_END:
+                new_code = TypeMarker.TURN_START
+
+            self.object_code = str(new_code)
+
+        # comments
+        if 'лево' in self.comments:
+            self.comments = self.comments.replace('лево', 'право')
+        elif 'право' in self.comments:
+            self.comments = self.comments.replace('право', 'лево')
+        elif 'начало' in self.comments:
+            self.comments = self.comments.replace('начало', 'конец')
+        elif 'конец' in self.comments:
+            self.comments = self.comments.replace('конец', 'начало')
