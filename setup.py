c5ae661dd2f92b4ac0a90a3edad5c0f3d7dbf465
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-12-10 21:21:46 +0400

    Pypi upload (#4)

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+For pypi.org upload
+"""
+import setuptools
+
+
+LONG = """
+# oeg.iv
+
+Python package for communication with OrgEnergoGaz InspectionViewer app
+"""
+
+setuptools.setup(
+  name='oeg_iv',
+  version='1.0',
+  author='Vitaly Bogomolov',
+  author_email='mail@vitaly-bogomolov.ru',
+  description='Python package for communication with OrgEnergoGaz InspectionViewer app',
+  long_description=LONG,
+  long_description_content_type="text/markdown",
+  url='https://github.com/vb64/oeg.iv',
+  packages=['oeg_iv', 'oeg_iv.csvfile'],
+  download_url='https://github.com/vb64/oeg.iv/archive/v1.0.tar.gz',
+  keywords=['python', 'OrgEnergoGaz', 'InspectionViewer', 'csv'],
+  classifiers=[
+    "Programming Language :: Python",
+    # https://autopilot-docs.readthedocs.io/en/latest/license_list.html
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+  ],
+)
